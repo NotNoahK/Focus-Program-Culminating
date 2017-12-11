1963bbd09dd747681b754879b87dc59dc6b1fae2
Aree Vanier <gregory@ajayinkingston.com>
2017-12-11 11:13:14 -0500

    Switched to using part class

---
+++ Assets/Part.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Part : MonoBehaviour{
+	
+	public bool working = true;
+	public bool attached = true;
+	public int health = 500;
+
+	void Start (){
+			
+	}
+
+
+}
+
